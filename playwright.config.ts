ace7c930ceff743bd9e42f0c0833cf100d01d10a
DfctPixel <bodik@gmail.com>
2026-06-26 21:06:03 +0300

    Bump workers to 10

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 1 : 0,
-  workers: 20,
+  workers: 10,
   reporter: [
     ['html', { outputFolder: 'playwright-report' }],
     ['list'],
